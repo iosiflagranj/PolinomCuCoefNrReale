1d9b0cb3f6d54d006240bbd4dd88cfe2370fa1e7
iosiflagranj <iosiflagranj@gmail.com>
2018-10-20 16:05:15 +0300

    Minor param changes

--- PolinomCuCoefNrReale/Polynomial.h
+++ PolinomCuCoefNrReale/Polynomial.h
@@ -5,11 +5,11 @@
 class Polynomial
 {
 public:
-	Polynomial(std::vector<double> = std::vector<double>(0));
+	Polynomial(std::vector<double> = std::vector<double>(1, 0));
 	~Polynomial();
 
 	const int get_degree();
-	void set_degree(int);
+	void set_degree(const int&);
 protected:
 private:
 	std::vector<double> m_coeff;
