907cce8667396a0516fbaf55edf4680d7b017d4f
iosiflagranj <iosiflagranj@gmail.com>
2018-10-20 15:48:09 +0300

    Added class members and function prototypes

--- PolinomCuCoefNrReale/Polynomial.h
+++ PolinomCuCoefNrReale/Polynomial.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <vector>
+#include <iostream>
+
 class Polynomial
 {
 public:
+	Polynomial(std::vector<double> = std::vector<double>(0));
+	~Polynomial();
+
+	const int get_degree();
+	void set_degree(int);
 protected:
 private:
+	std::vector<double> m_coeff;
+	int m_degree;
 };
 
