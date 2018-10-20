1b6e2a1e6a70ee280cc002ac81ab1b0ff8701e7e
iosiflagranj <iosiflagranj@gmail.com>
2018-10-20 16:05:58 +0300

    Defined constructor, degree getter and setter

--- PolinomCuCoefNrReale/Polynomial.cpp
+++ PolinomCuCoefNrReale/Polynomial.cpp
@@ -1,3 +1,23 @@
 #include "pch.h"
 #include "Polynomial.h"
 
+Polynomial::Polynomial(std::vector<double> coeff) :
+	m_coeff(coeff),
+	m_degree(coeff.size()-1)
+{
+}
+
+Polynomial::~Polynomial()
+{
+}
+
+const int Polynomial::get_degree()
+{
+	return m_degree;
+}
+
+void Polynomial::set_degree(const int& degree)
+{
+	this->m_degree = degree;
+}
+
